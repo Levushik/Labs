f84d81a15d6b90c7730568e4946f4a5497c181d9
Levik <lryskildin777@gmail.com>
2022-11-16 14:56:53 +0300

    Just the start

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        try(FileInputStream f = new FileInputStream("input.txt");
+            FileOutputStream o = new FileOutputStream("output.txt")) {
+            byte[] buffer = new byte[f.available()];
+            f.read(buffer,0, buffer.length);
+            o.write(buffer, 0, buffer.length);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
